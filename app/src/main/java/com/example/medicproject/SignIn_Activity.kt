bb97c6d661417db79a92689b498e2fa3619f97f8
Ntokozo <Ntokozoflex99@gmail.com>
2021-09-10 17:48:06 +0200

    LayOUT User

--- app/src/main/java/com/example/medicproject/SignIn_Activity.kt
+++ app/src/main/java/com/example/medicproject/SignIn_Activity.kt
@@ -2,13 +2,10 @@ package com.example.medicproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.firebase.ktx.Firebase
 
-class MainActivity : AppCompatActivity() {
-
+class SignIn_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sign_in_)
     }
-
 }
