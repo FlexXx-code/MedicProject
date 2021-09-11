3fc4bcf4de45eb49473449ce1ced5eae23c98b14
Ntokozo <Ntokozoflex99@gmail.com>
2021-09-11 15:05:43 +0200

    LayOUT User

--- app/src/main/java/com/example/medicproject/MainActivity.kt
+++ app/src/main/java/com/example/medicproject/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.medicproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +11,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val SignUpbutton=findViewById<Button>(R.id.signUp_btn)
+        val Logbutton=findViewById<Button>(R.id.loginBtn)
+
+
     }
 
 }
